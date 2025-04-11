1448bda28d3b5ed4445f631b52a11bcc3d86657c
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2025-04-11 21:01:31 +0100

    add: post creation function

--- src/pages/Homepage/Homepage Components/CreatePost.tsx
+++ src/pages/Homepage/Homepage Components/CreatePost.tsx
@@ -3,13 +3,32 @@ import writePostIcon from "../../../assets/edit-2.png";
 import uploadPhotoIcon from "../../../assets/gallery-add.png";
 import uploadVideoIcon from "../../../assets/video-circle.png";
 import { useState } from "react";
-
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../../../../config/firebase";
 type CreatePostProps = {
   isMenuOpen: boolean;
 };
 
 function CreatePost({ isMenuOpen }: CreatePostProps) {
   const [post,setPost] = useState<string>("");
+
+  const postCollections = collection(db, "posts")
+
+  const handlePost = async () => {
+    try {
+      if(post.trim() !== "") {
+        await addDoc(postCollections, {
+          postContent: post,
+          postHeader: "",
+          like: 0,
+          userId : auth?.currentUser?.uid
+        });
+        setPost("Post successfully added")
+      }
+    } catch(error) {
+      console.error(error)
+    }
+  }
   return (
     <article className="bg-White mx-1.5 py-6">
       <div className="border-b-Black/10 mx-1.5 flex justify-between border-b-[1px] pb-1.5">
@@ -26,6 +45,7 @@ function CreatePost({ isMenuOpen }: CreatePostProps) {
           className="focus:ring-IceBlue w-full py-5 pr-1.5 pl-9 focus:border-transparent focus:ring-2"
           value={post}
           onChange={(e) => setPost(e.target.value)}
+          
         />
         <img
           src={writePostIcon}
@@ -33,6 +53,7 @@ function CreatePost({ isMenuOpen }: CreatePostProps) {
           className="absolute top-1/2 left-2.5 -translate-y-1/2"
         />
       </div>
+      <button onClick={handlePost}>add post</button>
     </article>
   );
 }
