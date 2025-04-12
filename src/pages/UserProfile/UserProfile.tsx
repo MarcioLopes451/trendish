e858aa5bf1d9b6c183971be168b7b138e650639a
shaimaa <shisko939@gmail.com>
2025-04-12 14:29:49 +0200

    feat: add UserDetails component with gallery preview
    - Added 5x5 photo grid with upload for owners and navigation to /gallery.
    - Included details section with city, work/education, messages inputs.
    
    # Conflicts:
    #	src/App.tsx
    #	src/index.css

--- src/pages/UserProfile/UserProfile.tsx
+++ src/pages/UserProfile/UserProfile.tsx
@@ -1,9 +1,15 @@
 import { UserInfoPanel } from "./UserProfileComponents/UserInfoPanel";
+import { UserDetails } from "./UserProfileComponents/UserDetails";
+import Header from "../../ui/Header";
 
 export function UserProfile() {
   return (
-    <main className="bg-IceBlue font-sora h-screen flex flex-col items-center p-[40px]">
-      <UserInfoPanel />
+    <main >
+      <Header />
+      <div className="bg-IceBlue font-sora  flex flex-col items-center pt-[13px] px-[8px] lg:p-[40px] gap-[23px] lg:gap-[40px]">
+        <UserInfoPanel />
+        <UserDetails />
+      </div>
     </main>
   );
 }
--- src/pages/UserProfile/UserProfileComponents/UserInfoPanel.tsx
+++ src/pages/UserProfile/UserProfileComponents/UserInfoPanel.tsx
@@ -17,7 +17,7 @@ interface UserData {
 }
 
 interface UserInfoPanelProps {
-  userData?: UserData; 
+  userData?: UserData;
 }
 
 export function UserInfoPanel({ userData }: UserInfoPanelProps) {
@@ -37,42 +37,44 @@ export function UserInfoPanel({ userData }: UserInfoPanelProps) {
   };
 
   return (
-    <section className="w-[846px] flex flex-col bg-white shadow-[0px_2px_50px_0px_#A9DEF93D] rounded-[10px] pt-[37px]  px-[20px]">
+    <section className="w-full lg:w-[846px] flex flex-col bg-white shadow-[0px_2px_50px_0px_#A9DEF93D] rounded-[10px] pt-[12px] lg:pt-[37px] px-[6px] lg:px-[20px]">
       {/* User Info */}
-      <section className="flex items-center gap-[18px] pb-[29px] border-b border-black w-full">
-        <img
-          src={user.profileImage}
-          alt="User profile"
-          className="w-[78px] h-[83.02px]"
-        />
-        <div className="flex flex-col gap-[16px]">
-          <p className="text-[16px] tracking-0 leading-[100%] font-semibold text-Black">
+      <section className="flex flex-row items-center gap-[9px] lg:gap-[18px] pb-[15px] lg:pb-[29px] border-b border-black w-full">
+        <div className="w-[43px] h-[56px] overflow-hidden lg:w-[78px] lg:h-[83.02px] flex-shrink-0">
+          <img
+            src={user.profileImage}
+            alt="User profile"
+            className="w-full h-full object-cover "
+          />
+        </div>
+        <div className="flex flex-col gap-[5px] lg:gap-[16px]">
+          <p className=" text-[12px] lg:text-[16px] tracking-0 leading-[100%] font-semibold text-Black flex flex-col gap-[8px] lg:flex-row">
             {user.name}
-            <span className="opacity-50 text-[15.6px] font-normal pl-4">
+            <span className="opacity-50 lg:text-[15.6px] font-normal ">
               {user.username}
             </span>
           </p>
-          <p className=" tracking-0 leading-[100%] text-[15.6px] font-normal text-Black">
+          <p className=" tracking-0 leading-[100%] text-[12px] lg:text-[15.6px] font-normal text-Black">
             {user.bio}
           </p>
         </div>
       </section>
 
       {/* Actions and Stats */}
-      <section className="flex justify-between py-[23px]">
-        <div className="flex gap-[41px]">
+      <section className="flex justify-between gap-[18px] pt-[8px] pb-[20px] lg:py-[23px]">
+        <div className="flex flex-col lg:flex-row gap-[17px] lg:gap-[41px]">
           <button
-            className={`w-[163px] h-[40px] rounded-[8px] cursor-pointer bg-SkyBlue shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] text-black text-[15px] leading-[100%] tracking-0 font-bold `}
+            className={`w-[170px] h-[31px] lg:w-[163px] lg:h-[40px] rounded-[8px] cursor-pointer bg-SkyBlue shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] text-black text-[13px] lg:text-[15px] leading-[100%] tracking-0 font-bold `}
             onClick={() => setIsFriend(!isFriend)}
           >
             {isFriend ? "Friend Added" : "Add Friend"}
           </button>
-          <button className="w-[163px] h-[40px] rounded-[8px] cursor-pointer bg-LightGray shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] text-black text-[15px] leading-[100%] tracking-0 font-bold">
+          <button className="w-[170px] h-[31px] lg:w-[163px] lg:h-[40px] rounded-[8px] cursor-pointer bg-LightGray shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] text-black text-[13px] lg:text-[15px] leading-[100%] tracking-0 font-bold">
             Message
           </button>
         </div>
 
-        <div className="flex gap-[24px]">
+        <div className="flex gap-[6px] lg:gap-[24px]">
           {[
             { label: "Posts", count: user.stats.posts },
             { label: "Followers", count: user.stats.followers },
@@ -80,9 +82,9 @@ export function UserInfoPanel({ userData }: UserInfoPanelProps) {
           ].map((item, index) => (
             <div
               key={index}
-              className="flex flex-col justify-between items-center   leading-[100%] h-[52px]"
+              className="flex flex-col justify-between items-center   leading-[100%] h-[42px] lg:h-[52px] mt-[6px] lg:mt-0 "
             >
-              <p className="font-bold text-[24px] text-CharcoalBlue tracking-0 ">
+              <p className="font-bold text-[16px] lg:text-[24px] text-CharcoalBlue tracking-0 ">
                 {item.count}
               </p>
               <p className="text-SteelBlueGray font-normal text-[12px] tracking-[-0.4%]">
@@ -95,3 +97,5 @@ export function UserInfoPanel({ userData }: UserInfoPanelProps) {
     </section>
   );
 }
+
+   
