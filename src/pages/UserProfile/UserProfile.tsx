da0e644fd0b83d345ada6f2adbced9dd0ddb3d0f
shaimaa <shisko939@gmail.com>
2025-04-13 23:13:00 +0200

    solve confilect at index.css

--- src/pages/UserProfile/UserProfile.tsx
+++ src/pages/UserProfile/UserProfile.tsx
@@ -1,11 +1,15 @@
 import { UserInfoPanel } from "./UserProfileComponents/UserInfoPanel";
 import { UserDetails } from "./UserProfileComponents/UserDetails";
 import Header from "../../ui/Header";
+<<<<<<< HEAD
 import { Posts } from "./UserProfileComponents/Posts";
+=======
+>>>>>>> e858aa5 (feat: add UserDetails component with gallery preview)
 
 export function UserProfile() {
   return (
     <main >
+<<<<<<< HEAD
       <Header isMenuOpen={false} setIsMenuOpen={function (_isOpen: boolean): void {
         throw new Error("Function not implemented.");
       } } />
@@ -13,6 +17,12 @@ export function UserProfile() {
         <UserInfoPanel />
         <UserDetails />
         <Posts/>
+=======
+      <Header />
+      <div className="bg-IceBlue font-sora  flex flex-col items-center pt-[13px] px-[8px] lg:p-[40px] gap-[23px] lg:gap-[40px]">
+        <UserInfoPanel />
+        <UserDetails />
+>>>>>>> e858aa5 (feat: add UserDetails component with gallery preview)
       </div>
     </main>
   );
