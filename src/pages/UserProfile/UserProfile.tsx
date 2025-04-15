0eb86f7567b35143b6320104e5e70de23ed20eff
shaimaa <shisko939@gmail.com>
2025-04-15 18:07:28 +0200

    remove header form userProfile page

--- src/pages/UserProfile/UserProfile.tsx
+++ src/pages/UserProfile/UserProfile.tsx
@@ -1,14 +1,11 @@
 import { UserInfoPanel } from "./UserProfileComponents/UserInfoPanel";
 import { UserDetails } from "./UserProfileComponents/UserDetails";
-import Header from "../../ui/Header";
 import { Posts } from "./UserProfileComponents/Posts";
 
 export function UserProfile() {
   return (
     <main >
-      <Header isMenuOpen={false} setIsMenuOpen={function (_isOpen: boolean): void {
-        throw new Error("Function not implemented.");
-      } } />
+    
       <div className="bg-IceBlue font-sora  flex flex-col items-center py-[13px] px-[8px] lg:p-[40px] gap-[23px] lg:gap-[40px]">
         <UserInfoPanel />
         <UserDetails />
